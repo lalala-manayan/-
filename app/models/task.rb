97d8aeb05225f9086026627a02a900caaf681b7c
ManayaKimura <manayan3m3@icloud.com>
2020-09-10 15:53:36 +0900

    UserとTaskの紐付け

--- app/models/task.rb
+++ app/models/task.rb
@@ -2,6 +2,8 @@ class Task < ApplicationRecord
   validates :name, presence: true, length: {maximum: 30}
   validate :validate_name_not_including_comma
   before_validation :set_nameless_name
+  belongs_to :user
+  scope :recent, -> {order(created_at: :desc)}
 
   private
 
